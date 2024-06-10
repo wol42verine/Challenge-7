8a89c9a2d8ac7d644ad2ed39295c174581d007b9
wol42verine <49424009+wol42verine@users.noreply.github.com>
2024-06-10 13:11:44 -0700

    Added features

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-
-# testing
-
-
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+    # testing
+    
+    
 ## Table of Contents
 1. [Description](#description)
 2. [Installation](#installation)
@@ -11,28 +11,31 @@
 6. [License](#license)
 7. [FAQ](#faq)
 
+    
+    ## Description
+    testing
+    
+    ## Installation
+    testing
+    
+    ## Usage
+    testing
+    
+    ## Contributing
+    testing
 
-## Description
-testing
+For contributions, please visit [@wol42verine](https://github.com/wol42verine).
 
-## Installation
-testing
+You can also contact me at [yeah@hotmail.com](mailto:yeah@hotmail.com)
+    
+    ## Tests
+    testing
+    
+    ## License
+    Apache License 2.0
+    
+    ## FAQ
+    **Q1: "no questions"**
 
-## Usage
-testing
-
-## Contributing
-testing
-
-For contributions, please contact me at [testing@hotmail.com](mailto:testing@hotmail.com)
-
-## Tests
-testing
-
-## License
-testying
-
-## FAQ
-**Q1: "na"**
-
-**A1: "nah"**
+**A1: "no answers"**
+    
--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+// List of available licenses
+const licenses = [
+    {
+        name: 'MIT License',
+        badge: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+    },
+    {
+        name: 'Apache License 2.0',
+        badge: '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
+    },
+    // Add more licenses as needed
+];
+
 // Defining the questions for user input
 const questions = [
     {
@@ -30,6 +43,11 @@ const questions = [
     },
     {
         type: 'input',
+        name: 'githubUsername',
+        message: 'What is your GitHub username?',
+    },
+    {
+        type: 'input',
         name: 'email',
         message: 'What is your email address for contributions?',
     },
@@ -39,9 +57,10 @@ const questions = [
         message: 'How do you test your project?',
     },
     {
-        type: 'input',
+        type: 'list',
         name: 'license',
-        message: 'What license does your project have?',
+        message: 'Choose a license for your project:',
+        choices: licenses.map(license => license.name),
     },
     {
         type: 'confirm',
@@ -58,6 +77,7 @@ const questions = [
 
 // Prompt the user with the questions
 inquirer.prompt(questions).then((answers) => {
+    const selectedLicense = licenses.find(license => license.name === answers.license);
     const toc = answers.toc ? `
 ## Table of Contents
 1. [Description](#description)
@@ -83,35 +103,41 @@ inquirer.prompt(questions).then((answers) => {
         }
     }).join('\n\n');
 
+    // Format GitHub username as a hyperlink
+    const githubLink = answers.githubUsername ? `[@${answers.githubUsername}](https://github.com/${answers.githubUsername})` : '';
+
     const contributingSection = `${answers.contributing}
-${answers.email ? `\nFor contributions, please contact me at [${answers.email}](mailto:${answers.email})` : ''}`;
+${githubLink ? `\nFor contributions, please visit ${githubLink}.` : ''}
+${answers.email ? `\nYou can also contact me at [${answers.email}](mailto:${answers.email})` : ''}`;
 
-    const readmeContent = `
-# ${answers.title}
+    // Generate README content
+    const readmeContent = `${selectedLicense.badge || ''}
+    # ${answers.title}
+    
+    ${toc}
+    
+    ## Description
+    ${answers.description}
+    
+    ## Installation
+    ${answers.installation}
+    
+    ## Usage
+    ${answers.usage}
+    
+    ## Contributing
+    ${contributingSection}
+    
+    ## Tests
+    ${answers.tests}
+    
+    ## License
+    ${answers.license}
+    
+    ## FAQ
+    ${faqEntries}
+    `;
 
-${toc}
-
-## Description
-${answers.description}
-
-## Installation
-${answers.installation}
-
-## Usage
-${answers.usage}
-
-## Contributing
-${contributingSection}
-
-## Tests
-${answers.tests}
-
-## License
-${answers.license}
-
-## FAQ
-${faqEntries}
-`;
 
     // Write the README content to a README.md file
     fs.writeFile('README.md', readmeContent, (err) => {
@@ -122,3 +148,4 @@ ${faqEntries}
         console.log('README.md has been generated!');
     });
 });
+//Todo: TOC links, Github link, Badge Icon
